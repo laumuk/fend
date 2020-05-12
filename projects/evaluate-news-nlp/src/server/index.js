bbea55a0695b40ce3a6b6b000dfea0f1f6309a4c
Laura Bobrovacz <bobrovacz.laura@gmail.com>
2020-05-12 19:37:22 +0200

    fixing functionality

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,6 +1,15 @@
-var path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const aylien = require('aylien_textapi')
+
+const textapi = new aylien({
+    application_id: `${process.env.API_ID}`,
+    application_key: `${process.env.API_KEY}`
+});
 
 const app = express()
 
