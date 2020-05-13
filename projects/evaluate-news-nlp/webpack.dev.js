60d450ba38e385aae574fa746184cb63b87bfff8
Laura Bobrovacz <bobrovacz.laura@gmail.com>
2020-05-13 16:53:34 +0200

    adding service worker

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -1,8 +1,8 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path'),
+const webpack = require('webpack'),
+const HtmlWebPackPlugin = require("html-webpack-plugin"),
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     entry: './src/client/index.js',
@@ -30,7 +30,7 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
-            filename: "./index.html",
+            filename: "./index.html"
         }),
         new CleanWebpackPlugin({
             // Simulate the removal of files
--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,15 +1,15 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
+const path = require('path'),
+const webpack = require('webpack'),
+const HtmlWebPackPlugin = require("html-webpack-plugin"),
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
+const TerserPlugin = require('terser-webpack-plugin'),
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
-    
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
@@ -22,15 +22,19 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
-            filename: "./index.html",
+            filename: "./index.html"
         }),
-        new MiniCssExtractPlugin({filename: '[name].css'})
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true
+        })
     ]
 }
